26add832dcb6511ac58c5109ccddc7550c861f94
Maxime <maxime.borry@gmail.com>
2017-10-27 14:57:11 +0200

    add resdpeth function

--- scripts/resdepth.py
+++ scripts/resdepth.py
@@ -9,7 +9,7 @@ rd = ResidueDepth(model, '2za4.pdb')
 mydict = {}
 
 for item in rd.property_list:
-   
+
     # Create a tuple => (chain, residue3LetterCode, Id)
     residue = (
                 item[0].get_parent().id,      # Chain
@@ -17,8 +17,13 @@ for item in rd.property_list:
                 item[0].get_id()[1]              # Position in chain
               )
     result = item[1]                                # (ResidueDepth, CalphaDepth)
-    
+
     # Stores everything in a dict
     mydict[residue] = result
 
 print (mydict)
+
+
+def calc_depth(depth_dict, residue, thresh):
+    depth = depth_dict[residue.get_full_id()[2:4]][1]
+    return(depth)
