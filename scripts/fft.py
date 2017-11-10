c411cfaf5cad14298a3bb6cdd6544563b58d9934
Maxime <maxime.borry@gmail.com>
2017-11-10 15:40:59 +0100

    try with np.matmut

--- scripts/fft.py
+++ scripts/fft.py
@@ -7,6 +7,7 @@ from numpy.fft import *
 
 
 def make_fft(rec_grid, lig_grid, L):
+    # tmp = ((1/L**3) *ifftn(np.matmul(ifftn(rec_grid),fftn(lig_grid))))
     tmp = ((1/L**3) *ifftn(np.dot(ifftn(rec_grid),fftn(lig_grid))))
     sc = tmp.real - tmp.imag
     return(sc)
