e6bca021a52415d1cf1ddd5b544b0d44c22c34e2
Maxime <maxime.borry@gmail.com>
2017-11-15 16:40:16 +0100

    fft update

--- scripts/fft.py
+++ scripts/fft.py
@@ -6,11 +6,20 @@ import math
 from numpy.fft import *
 
 
-def make_fft(rec_grid, lig_grid, L):
-    # tmp = ((1/L**3) *ifftn(np.matmul(ifftn(rec_grid),fftn(lig_grid))))
-    tmp = ((1/L**3) *ifftn(np.dot(ifftn(rec_grid),fftn(lig_grid))))
+def make_fft(rec_grid, lig_grid):
+    L = rec_grid.shape[0]
+    tmp = ((1/L**3) *ifftn(np.matmul(ifftn(rec_grid),fftn(lig_grid))))
+    # tmp = ((1/L**3) *ifftn(np.dot(ifftn(rec_grid),fftn(lig_grid))))
     sc = tmp.real - tmp.imag
+    sc = sc.sum()
+    # sc = sc.real
     return(sc)
+
+def multi_mat(rec_grid, lig_grid):
+    res = np.matmul(rec_grid, lig_grid)
+    res = res.sum()
+    # res = res.real
+    return(res)
 
 
 
