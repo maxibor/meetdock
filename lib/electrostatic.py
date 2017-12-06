631dbf753ee3ab9e144336be4164d9a81043bca9
Maxime <maxime.borry@gmail.com>
2017-12-06 14:48:07 +0100

    electro update

--- lib/electrostatic.py
+++ lib/electrostatic.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+
+
+import pdbtools
+import matrice_distances
+import pdb_resdepth
+import sys, math
+
 def amino3to1(resid):
     codes = {
     "ALA":"A",
@@ -18,7 +26,8 @@ def amino3to1(resid):
     "CYS":"C",
     "MET":"M",
     "ASN":"N",
-    "GLN":"Q"
+    "GLN":"Q",
+    "GLY":"G"
     }
     return(codes[resid])
 
@@ -32,8 +41,9 @@ def calc_charge(resid, pH):
         OUTPUT:
             - charge(float) charge of the amino acid
     """
-
-    resid_letter = amino3to1(resid[1])
+    # print(resid)
+    # resid_letter = amino3to1(resid[1])
+    resid_letter = resid
 
     resPka = {
     "Y":[+1,10.46],
@@ -46,7 +56,7 @@ def calc_charge(resid, pH):
     }
 
     qi = resPka[resid_letter][0]
-    pka = resPka[resid_letter][1]
+    pKa = resPka[resid_letter][1]
     charge = qi/(1+10**(qi*(pH-pKa)))
     return(charge)
 
@@ -65,30 +75,42 @@ def calc_electrostat(charge1, charge2, distance):
     return(res)
 
 
-def electrostatic(inter_resid_dict, pH):
+def electrostatic(inter_resid_dict, pH = 7):
     """
         Computes sum of electrostatic energies between ligand and receptor
         INPUT:
             - inter_resid_dict(dict) dict of distances between residues at
             interface
-            - pH(float) pH for charge calculation
+            - pH(float) pH for charge calculation. Default = 7
         OUTPUT:
             -
 
     """
     elec_sum = 0
+    electro_amino = ["Y","H","C","D","Z","K","R"]
     for akey in inter_resid_dict.keys():
         residcode1 = amino3to1(akey[0][1])
         residcode2 = amino3to1(akey[1][1])
-        charge1 = calc_charge(residcode1, pH)
-        charge2 = calc_charge(residcode2, pH)
-        elec_sum += calc_electrostat(charge1=charge1, charge2=charge2, distance = inter_resid_dict[akey])
+        if (residcode1 in electro_amino) and (residcode2 in electro_amino):
+            # print(residcode1, residcode2), inter_resid_dict[akey]
+            charge1 = calc_charge(residcode1, pH)
+            charge2 = calc_charge(residcode2, pH)
+            elec_sum += calc_electrostat(charge1=charge1, charge2=charge2, distance = inter_resid_dict[akey])
     return(elec_sum)
 
 
+
+
 if __name__ == "__main__":
-    filename = sys.argv[1]
-    pdb_structure = read_pdb(filename)
-    depth_dict = pdb_resdepth.calculate_resdepth(structure=pdb_structure, pdb_filename=filename)
-    # TODO then distance matrix and cutoff from Paula
-    
+    """
+    Usage for testing
+        electrostatic.py path/to/complex.pdb recepChain1,recepChainN ligChain1,ligChainN
+    """
+    myfile = sys.argv[1]
+    recepChain = sys.argv[2].split(",")
+    ligChain = sys.argv[3].split(",")
+    my_struct = pdbtools.read_pdb(myfile)
+    depth_dict = pdb_resdepth.calculate_resdepth(structure=my_struct, pdb_filename=myfile)
+    inter_resid = matrice_distances.calc_distance_matrix(structure=my_struct, depth= depth_dict, chain_R=recepChain, chain_L=ligChain)
+    myElectro = electrostatic(inter_resid_dict=inter_resid, pH=7)
+    print(myElectro)
