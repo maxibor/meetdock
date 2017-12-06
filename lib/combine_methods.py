d5e7aacc5380f7794ee213c9f63fe94fc76976e4
Maxime <maxime.borry@gmail.com>
2017-12-06 16:39:18 +0100

    combine score

--- lib/combine_methods.py
+++ lib/combine_methods.py
@@ -9,23 +9,17 @@ import electrostatic
 import shape_complement
 import knowledge
 
-
-# myfile = sys.argv[1]
-# recepChain = sys.argv[2].split(",")
-# ligChain = sys.argv[3].split(",")
-# my_struct = pdbtools.read_pdb(myfile)
-# depth_dict = pdb_resdepth.calculate_resdepth(structure=my_struct, pdb_filename=myfile)
-# inter_resid = matrice_distances.calc_distance_matrix(structure=my_struct, depth= depth_dict, chain_R=recepChain, chain_L=ligChain)
-
 def combine_score(pdbfile, recepChain, ligChain):
     combined_dict = {}
     my_struct = pdbtools.read_pdb(pdbfile)
     depth_dict = pdb_resdepth.calculate_resdepth(structure=my_struct, pdb_filename=pdbfile)
     distmat = matrice_distances.calc_distance_matrix(structure=my_struct, depth= depth_dict, chain_R=recepChain, chain_L=ligChain)
+
     statpot = knowledge.parse_distance_mat(distmat, method=["glaser"])
     vdw = Lennard_Jones.lennard_jones(dist_matrix=distmat)
     electro = electrostatic.electrostatic(inter_resid_dict=distmat)
     shape = shape_complement.runshape(structure=my_struct, recepChain=recepChain, depth_dict=depth_dict, ligChain=ligChain)
+    # foldx = TOADD
     combined_dict["statpot"] = statpot
     combined_dict["vdw"] = vdw
     combined_dict["electro"] = electro
@@ -40,16 +34,3 @@ if __name__ == '__main__':
     ligChain = sys.argv[3].split(",")
     mydict = combine_score(pdbfile=myfile, recepChain=recepChain, ligChain=ligChain)
     print(mydict)
-
-
-# 1 - read_pdb
-# 2 - resdepth
-# 3 - matrice de distances
-# 4 - knowledge
-# 5 - Lennard
-# 6 - Electrostatic
-# 7 - Shape complement
-# 8 - combiner en dictionary
-#
-#
-# nom_du_pdb = {"method1":scoreMethod1, "methodN":scoreMethodeN}
