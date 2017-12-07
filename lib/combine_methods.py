01032d47204a5682d3277f9610f882e263368eb1
Maxime <maxime.borry@gmail.com>
2017-12-07 15:38:46 +0100

    update combine methods

--- lib/combine_methods.py
+++ lib/combine_methods.py
@@ -23,16 +23,20 @@ import warnings
 from Bio import BiopythonWarning
 warnings.simplefilter('ignore', BiopythonWarning)
 
-def combine_score(pdbfile, recepChain, ligChain):
+def combine_score(pdbfile, recepChain, ligChain, statpotrun, vdwrun, electrorun, shaperun, pH, depth, dist):
     combined_dict = {}
     my_struct = pdbtools.read_pdb(pdbfile)
     depth_dict = pdb_resdepth.calculate_resdepth(structure=my_struct, pdb_filename=pdbfile)
-    distmat = matrice_distances.calc_distance_matrix(structure=my_struct, depth= depth_dict, chain_R=recepChain, chain_L=ligChain)
+    distmat = matrice_distances.calc_distance_matrix(structure=my_struct, depth= depth_dict, chain_R=recepChain, chain_L=ligChain, dist_max=dist)
 
-    statpot = knowledge.parse_distance_mat(distmat, method=["glaser"])
-    vdw = Lennard_Jones.lennard_jones(dist_matrix=distmat)
-    electro = electrostatic.electrostatic(inter_resid_dict=distmat)
-    shape = shape_complement.runshape(structure=my_struct, recepChain=recepChain, depth_dict=depth_dict, ligChain=ligChain)
+    if statpotrun == True:
+        statpot = knowledge.parse_distance_mat(distmat, method=["glaser"])
+    if vdwrun == True:
+        vdw = Lennard_Jones.lennard_jones(dist_matrix=distmat)
+    if electrorun == True:
+        electro = electrostatic.electrostatic(inter_resid_dict=distmat, pH =pH)
+    if shaperun == True:
+        shape = shape_complement.runshape(structure=my_struct, recepChain=recepChain, depth_dict=depth_dict, ligChain=ligChain, depthCutoff =depth)
     # foldx = TOADD
     combined_dict["statpot"] = statpot
     combined_dict["vdw"] = vdw
