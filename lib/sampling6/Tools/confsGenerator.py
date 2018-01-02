9d7c31f95567bda03ef7e583e49f163a0d47302d
Maxime Borry <maxibor@users.noreply.github.com>
2018-01-02 22:55:44 +0100

    Update confsGenerator.py

--- lib/sampling6/Tools/confsGenerator.py
+++ lib/sampling6/Tools/confsGenerator.py
@@ -25,11 +25,11 @@ if __name__ == '__main__':
 	parser.add_argument("-minimize", required=False, default="True", help="Par défaut, les PDB ne sont pas minimisés. Changer à True pour minimisation.")
 	parser.add_argument("-jet", required=False, default="False", help="Lance le sampling avec Jet ou naif par défaut.")
 	args = parser.parse_args()
-	
+
 	if args.p == None or args.r == None or args.l == None: #si l'un des arguments obligatoires est vide
 		parser.print_help()
 		parser.exit()
-	
+
 	filename_rec=os.path.basename(args.r)
 	filename_lig=os.path.basename(args.l)
 	nbPt=int (args.nbPt)
@@ -46,7 +46,7 @@ if __name__ == '__main__':
 
 	print ("receptor pdb:", filename_rec)
 	print ("ligand pdb:",filename_lig)
-	
+
 	#### Vérif '-jet' ####
 	if args.jet == "True":
 		Jet=True
@@ -60,7 +60,7 @@ if __name__ == '__main__':
 		mini=True
 	elif args.minimize =="False":
 		mini=False
-		
+
 	##########################
 
 
@@ -68,60 +68,60 @@ if __name__ == '__main__':
 
 	recepteur = st.parsePDBMultiChains(args.r)
 	ligand = st.parsePDBMultiChains(args.l)
-	
+
 	print ("\n\n######################################################")
 	print("Deplacement des proteines à leur position initiales :")
 	print("######################################################")
 	cdm_origin = st.cdm(recepteur)
 	st.cdm(ligand)
-	trans_origin = st.translation(recepteur,cdm_origin["x"],cdm_origin["y"],cdm_origin["z"])                                      
-	distanceRL = recepteur['cdm']['r']+ligand['cdm']['r']+DIST_CONTACT               
-	st.translation(ligand,distanceRL,cdm_origin["y"],cdm_origin["z"])  
+	trans_origin = st.translation(recepteur,cdm_origin["x"],cdm_origin["y"],cdm_origin["z"])
+	distanceRL = recepteur['cdm']['r']+ligand['cdm']['r']+DIST_CONTACT
+	st.translation(ligand,distanceRL,cdm_origin["y"],cdm_origin["z"])
 	print("Calcul CDM du recepteur apres translation : "); st.cdm(recepteur,True)
 	print("Calcul CDM du ligand apres translation : "); st.cdm(ligand,True)
-	
+
 	#essayer de creer un dossier 'Results'
 	try:
 		os.mkdir("%s"%pathResults)
 	except:
 		pass
-	
+
 	#essayer de creer un dossier 'ptDepart' dans le dossier 'Results'
 	try:
 		os.mkdir("%s/ptDepart"%pathResults)
 	except:
 		pass
-	
+
 	#dans le dossier 'Results/ptDepart', essayer de supprimer le pdb de point de depart correspondant a la proteine actuellement en traitement (pour enlever la sortie de l'ancienne execution)
 	try:
 		os.remove("%s/ptDepart/%s_ptDepart.pdb"%(pathResults,filename_rec.split("_")[0]))
 	except:
 		pass
-			
-	#essayer de creer dans le dossier 'Results' un dossier 'pdb' pour stocker les fichiers de sorties non minimises	
+
+	#essayer de creer dans le dossier 'Results' un dossier 'pdb' pour stocker les fichiers de sorties non minimises
 	try:
 		os.mkdir("%s/pdb"%pathResults)
 	except:
 		pass
-	
-	#essayer de supprimer le dossier concernant la proteine actuelle (dossier issu de l'ancienne execution)	
+
+	#essayer de supprimer le dossier concernant la proteine actuelle (dossier issu de l'ancienne execution)
 	try:
 		shutil.rmtree("%s/pdb/%s"%(pathResults,filename_rec.split("_")[0]))
 	except OSError:
 		pass
-	
+
 	os.mkdir("%s/pdb/%s"%(pathResults,filename_rec.split("_")[0]))
-	
+
 	#ecrire le pdb recepteur (le recepteur positionne a l'origine)
 # 	recepteur2 = recepteur
 # 	st.translation2(recepteur2,trans_origin[0],trans_origin[1],trans_origin[2])
-# 	st.writePDB(recepteur2,filout="%s/pdb/%s/%s"%(pathResults,filename_rec.split("_XX")[0],filename_rec)) 
-	st.writePDB(recepteur,filout="%s/pdb/%s/%s"%(pathResults,filename_rec.split("_")[0],filename_rec))  	                        
+# 	st.writePDB(recepteur2,filout="%s/pdb/%s/%s"%(pathResults,filename_rec.split("_XX")[0],filename_rec))
+	st.writePDB(recepteur,filout="%s/pdb/%s/%s"%(pathResults,filename_rec.split("_")[0],filename_rec))
 
 	print ("\n\n######################################################")
 	print("Rotation du ligand autour du récepteur:")
 	print("######################################################")
-	
+
 
 	# Lancement avec ou sans Jet2
 	if(Jet==False):
@@ -134,9 +134,9 @@ if __name__ == '__main__':
 		#~ os.system('export JET2_PATH="%s/JET2/"'%(prog_path))
 		#~ shutil.copyfile(args.r,os.path.join(prog_path,"/JET2"))
 		#~ os.system("java -cp $JET2_PATH:$JET2_PATH/jet/extLibs/vecmath.jar jet.JET -c ./JET2/default.conf -i \"./JET2/%s\" -o Results/Jet_Output -p AVJC -r local -a 3 -d chain"%(filename_rec))
-		
+
 		os.environ["JET2_PATH"]="%s/JET2/"%prog_path
-		
+
 		try:
 			os.mkdir("%s/Jet_Output"%pathResults)
 		except:
@@ -158,13 +158,13 @@ if __name__ == '__main__':
 		(phi,theta)=st.selectPtDepartBis(recepteur["cdm"], distanceRL,(phi,theta),args.r, recepteur)
 		print(len(phi)," points de départ sélectionnés.")
 
-		
+
 		#~ try:
 			#~ print("./JET2/%s"%(filename_rec))
 			#~ os.remove("./JET2/%s"%(filename_rec))
 		#~ except OSError:
 			#~ pass
-			
+
 		#essayer de creer un dossier 'Jet_Output' dans le dossier 'Results'
 
 		###
@@ -172,15 +172,15 @@ if __name__ == '__main__':
 
 	coord=(recepteur['cdm']['x'],recepteur['cdm']['y'],recepteur['cdm']['z'])
 	cb=0
-	
+
 	pas_alpha=int(math.ceil(360/XRota))
 	pas_beta=int(math.ceil(360/YRota))
 	pas_gamma=int(math.ceil(360/ZRota))
-	
+
 	list_ptDepart=[]
 	for i in range(len(phi)):
 		ptDepart=st.spherical2CartCoord((coord[0],coord[1],coord[2]),distanceRL,phi[i],theta[i])
-		st.translation(ligand,ptDepart[0],ptDepart[1],ptDepart[2]) 
+		st.translation(ligand,ptDepart[0],ptDepart[1],ptDepart[2])
 		list_ptDepart.append([ptDepart[0],ptDepart[1],ptDepart[2]])
 		st.cdm(ligand)
 		x0=ligand["cdm"]["x"]
@@ -194,16 +194,16 @@ if __name__ == '__main__':
 					st.cdm(ligand)
 					st.selfRotation(ligand,alpha,beta,gamma)
 					st.rapproche(recepteur,ligand)
-# 					st.translation2(ligand, trans_origin[0],trans_origin[1],trans_origin[2]) 
+# 					st.translation2(ligand, trans_origin[0],trans_origin[1],trans_origin[2])
 					st.addPDB(ligand,cb,filout="%s/pdb/%s/%s_rota%s.pdb"%(pathResults,filename_rec.split("_")[0],filename_lig.split(".")[0],cb))
 					cb+=1
-					
+
 					print("*",end='')
-# 	st.writePDB(recepteur2,filout="%s/pdb/%s/%s"%(pathResults,filename_rec.split("_")[0],filename_rec)) 
+# 	st.writePDB(recepteur2,filout="%s/pdb/%s/%s"%(pathResults,filename_rec.split("_")[0],filename_rec))
 	#print("%s/pdb/%s/%s"%(pathResults,filename_rec.split("_")[0],filename_rec))
 	#ecrire fichier pdb pour les points de depart
-	st.ptDepartPDB(list_ptDepart,"%s/ptDepart/%s_ptDepart.pdb"%(pathResults,filename_rec.split("_")[0]))				
-	
+	st.ptDepartPDB(list_ptDepart,"%s/ptDepart/%s_ptDepart.pdb"%(pathResults,filename_rec.split("_")[0]))
+
 	#### minimisation des output ####
 	if mini == True:
 # 		os.system("python3 %s/Tools/loopMini.py -r %s"%(prog_path,filename_rec))
@@ -211,14 +211,6 @@ if __name__ == '__main__':
 	else:
 		print("Traitement terminé !")
 	#################################"""
-	
+
 	#tracer le graphe pyplot des points de depart
-	#st.graph_ptDepart(list_ptDepart,recepteur)
-	
-	
-		
-
-			
-
-			
-		
+	# st.graph_ptDepart(list_ptDepart,recepteur)
